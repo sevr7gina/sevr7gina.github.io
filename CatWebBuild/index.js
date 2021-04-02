e10d3756395d68caa604bf2e0d9d6c10a24f77d0
sevr7gina <sevr7gina@mail.ru>
2021-04-03 02:53:43 +0300

    53

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -2,4 +2,15 @@ function ShowAds() {
 	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 	.then(data => console.log(data.result))
 	.catch(error => console.log(error));
+	
+	
+	  VK.init(function() {
+      // API initialization succeeded
+      // Your code here
+	  VK.callMethod('showSettingsBox', 256);
+	  
+  }, function() {
+     // API initialization failed
+     // Can reload page here
+}, '5.60');
 	}	
