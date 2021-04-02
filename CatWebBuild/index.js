7c569a3c1346555557d4f5aa919128e29a915c76
sevr7gina <sevr7gina@mail.ru>
2021-04-02 14:03:15 +0300

    Update index.js

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -1,5 +1,5 @@
 function ShowAds() {
-	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"preloader"})
+	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 	.then(data => console.log(data.result))
 	.catch(error => console.log(error));
 	}	
