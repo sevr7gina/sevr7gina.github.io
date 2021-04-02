fa768e22229009b04fc90f7d7429b3ef1574e667
sevr7gina <sevr7gina@mail.ru>
2021-04-03 02:41:33 +0300

    98

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -7,7 +7,13 @@ function ShowAds() {
 	  VK.init(function() {
       // API initialization succeeded
       // Your code here
-	  VK.callMethod('showSettingsBox', 256);
+	  //VK.callMethod('showSettingsBox', 256);
+	  admanInit({
+      user_id: user_id,
+      app_id: 7781392,
+      type: 'preloader'         // 'preloader' or 'rewarded' (default - 'preloader')
+      // params: {preview: 1}   // to verify the correct operation of advertising
+    }, onAdsReady, onNoAds);
   }, function() {
      // API initialization failed
      // Can reload page here
