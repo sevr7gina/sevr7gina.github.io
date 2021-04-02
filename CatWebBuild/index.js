2bb6db29e72fff1af593b283a520a95778292cce
sevr7gina <sevr7gina@mail.ru>
2021-04-03 01:28:36 +0300

    1

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -2,4 +2,5 @@ function ShowAds() {
 	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 	.then(data => console.log(data.result))
 	.catch(error => console.log(error));
+	VK.callMethod('showSettingsBox', 263);
 	}	
