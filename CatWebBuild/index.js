eab5851c96cec2e73eea4f670f501eb2deb98005
sevr7gina <sevr7gina@mail.ru>
2021-04-03 02:59:45 +0300

    5

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -3,5 +3,10 @@ function ShowAds() {
 	.then(data => console.log(data.result))
 	.catch(error => console.log(error));
 	
-	vkBridge.send("VKWebAppShowWallPostBox", {"message": "Hello!"});
+	//vkBridge.send("VKWebAppShowWallPostBox", {"message": "Hello!"});
+	
+	vkBridge.send("VKWebAppShowWallPostBox", {
+  "message": "Hello!",
+  "attachments": "https://vk.com/app7781392"
+});
 	}	
