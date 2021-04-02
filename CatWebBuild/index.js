a62a08fdd69ab79838987b08e62fedfaccddd2c1
sevr7gina <sevr7gina@mail.ru>
2021-04-02 16:16:58 +0300

    16

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -1,20 +1,5 @@
 function ShowAds() {
-	var user_id = null;   // user's id
-    var app_id = 7781392;  // your app's id
- 
-    admanInit({
-      user_id: user_id,
-      app_id: 7781392,
-      type: 'preloader'         // 'preloader' or 'rewarded' (default - 'preloader')
-      // params: {preview: 1}   // to verify the correct operation of advertising
-    }, onAdsReady, onNoAds);
- 
-    function onAdsReady(adman) {
-      adman.onStarted(function () {});
-      adman.onCompleted(function() {});          
-      adman.onSkipped(function() {});          
-      adman.onClicked(function() {});
-      adman.start('preroll');
-    };
-    function onNoAds() {};
+	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+	.then(data => console.log(data.result))
+	.catch(error => console.log(error));
 	}	
