a9ed826e14c157011618fb6e6c22d7ac737246a5
sevr7gina <sevr7gina@mail.ru>
2021-04-03 03:36:54 +0300

    5

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -3,10 +3,12 @@ function ShowAds() {
 	.then(data => console.log(data.result))
 	.catch(error => console.log(error));
 	
-	//vkBridge.send("VKWebAppShowWallPostBox", {"message": "Hello!"});
+	//vkBridge.send("VKWebAppShowWallPostBox", {
+	//"message": "Hello!",
+	//"attachments": "https://vk.com/app7781392"  
+	//});
 	
-	vkBridge.send("VKWebAppShowWallPostBox", {
-  "message": "Hello!",
-  "attachments": "photo275324683_457240214","https://vk.com/app7781392"
-});
+	vkBridge.send("VKWebAppShowInviteBox", {})
+         .then(data => console.log(data.success))
+        .catch(error => console.log(error));
 	}	
