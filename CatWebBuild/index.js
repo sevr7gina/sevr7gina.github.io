f6c0a17ec284b0225def9b9f11ab68a4c44bd749
sevr7gina <sevr7gina@mail.ru>
2021-04-03 13:54:49 +0300

    new functions and share button in Unity

--- CatWebBuild/index.js
+++ CatWebBuild/index.js
@@ -1,17 +1,25 @@
 function ShowAds() {
+	//show ads on mobile only
 	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 	.then(data => console.log(data.result))
 	.catch(error => console.log(error));
 	
+	//show pop up for adding app to favorites on web/mobile	
+	vkBridge.send("VKWebAppAddToFavorites");
+	}	
+	
+function ShowWallPostBox() {
+	//show pop up for adding post about app to player's wall on web/mobile	
 	vkBridge.send("VKWebAppShowWallPostBox", {
 	"message": "Прыгай по столбикам и получай очки. Сможешь набрать больше всех?!",
 	"attachments": "https://vk.com/app7781392"  
 	});
+	}
 	
-	//vkBridge.send("VKWebAppShowInviteBox", {})
-    //     .then(data => console.log(data.success))
-    //    .catch(error => console.log(error));
-		
-		
-	vkBridge.send("VKWebAppAddToFavorites");
-	}	
+function ShowInviteBox() {
+	//show pop up for inviting friends to app on web/mobile	
+	vkBridge.send("VKWebAppShowInviteBox", {})
+    .then(data => console.log(data.success))
+    .catch(error => console.log(error));
+	}
+	
